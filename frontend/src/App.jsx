36fc4a01256bbfc8ded6bf42223928113bda0086
kaiser1x <qdtran.dev@gmail.com>
2025-10-13 00:53:14 -0400

    Fix: Resolved package.json duplicates and App.jsx merge conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,16 +27,8 @@ function App() {
   return (
     <div className="app">
       {renderPage()}
-import Home from './pages/Home'
-import './App.css'
-
-function App() {
-  return (
-    <div className="app">
-      <Home />
     </div>
   )
 }
 
 export default App
-
